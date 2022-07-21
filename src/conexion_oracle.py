c4249dbf5db0de61de474b999c5685ec573b9993
Neider <ricsonearest@gmail.com>
2022-07-20 22:46:22 -0500

    conection oracle

---
+++ src/conexion_oracle.py
@@ -0,0 +1,27 @@
+
+import cx_Oracle
+
+try:
+    connection=cx_Oracle.connect(
+        user='MODULODEP',
+        password='1234',
+        dsn='localhost:1521/XE',
+        encoding='UTF-8'
+    )
+    print(connection.version)
+    cursor=connection.cursor()
+    # cursor.execute("INSERT INTO ROL (IDROL, DESCROL) VALUES ('3', 'Pasante')")
+    # cursor.execute("UPDATE ROL SET DESCROL = 'Pasante' WHERE IDROL = 2")
+    # cursor.execute("UPDATE ROL SET DESCROL = 'Entrenador' WHERE IDROL = 3")
+    connection.commit()
+    cursor.execute("SELECT * FROM ROL")
+    rows=cursor.fetchall()
+    for row in rows:
+        print(row)
+except Exception as ex:
+    print("Exeption")
+    print(ex)
+finally:
+    connection.close()
+    print("Conexión Finalizada")
+
