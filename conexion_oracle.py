5d45b07d6a076907fb327108cfb4ac96cb44f398
valentinatobo <valentina.uscategui99@gmail.com>
2022-07-24 18:24:40 -0500

    Arreglo logica select

--- conexion_oracle.py
+++ conexion_oracle.py
@@ -4,27 +4,8 @@ from flask import Flask, render_template, request, redirect, request_started, ur
 from jinja2 import Environment, FileSystemLoader
 import os
 app = Flask(__name__)
-# try:
-#     connection=cx_Oracle.connect(
-#         user='MODULODEP',
-#         password='1234',
-#         dsn='localhost:1521/XE',
-#         encoding='UTF-8'
-#     )
-#     cursor=connection.cursor()
-#     # cursor.execute("INSERT INTO ROL (IDROL, DESCROL) VALUES ('3', 'Pasante')")
-#     # cursor.execute("UPDATE ROL SET DESCROL = 'Pasante' WHERE IDROL = 2")
-#     # cursor.execute("UPDATE ROL SET DESCROL = 'Entrenador' WHERE IDROL = 3")
-#     connection.commit()
-#     cursor.execute("SELECT * FROM ROL")
-#     rows=cursor.fetchall()
-#     for row in rows:
-#         print(row)
-# except Exception as ex:
-#     print(ex)
-# finally:
-#     connection.close()
-#     print("Conexión Finalizada")
+
+#obtiene los roles y sedes
 def getInfoSelec():
     try:
         cdtls = get_credentials()
@@ -42,6 +23,7 @@ def getInfoSelec():
         connection.close()
     return rols, sedes
 
+#obtiene el rol y la sede del id
 def getInfo(id):
     try:
         cdtls = get_credentials()
@@ -97,7 +79,7 @@ def insert_default():
         col+="Conexión Finalizada"
     return col
 
-#Consulta tabla
+#Consulta informacion personal
 @app.route('/Personal')
 def get_personal():
     try:
@@ -117,11 +99,13 @@ def get_personal():
         connection.close()
     return render_template('tabla.html', contacts=rows)
 
+#Agrega sedes y roles al form
 @app.route('/addPersonal')
 def personal_form():
     info = getInfoSelec();
     return render_template('form.html', infoRols = info[0], infoSedes = info[1])
 
+#Crear nuevo personal
 @app.route('/addPersonal', methods={'POST'})
 def insert_personal():
     if request.method=='POST':
@@ -146,6 +130,7 @@ def insert_personal():
         connection.close()
     return get_personal()
 
+#obtener la información del usuario segun su id
 @app.route('/edit/<string:id>')
 def edit_form(id):
     try:
@@ -165,17 +150,15 @@ def edit_form(id):
         connection.close()
     info = getInfoSelec();
     get_Info =  getInfo(id);
-    print(get_Info)
     return render_template('editUser.html', contact=rows,infoRols = info[0], infoSedes = info[1], infoSelec = get_Info)
 
+#editar la información del usuario en la bd
 @app.route('/edit/<string:id>',  methods={'POST'})
 def preubasUpdate(id):
     if request.method=='POST':
         id=request.form['idPersonal']
         nombre=request.form['nombre']
         apellido=request.form['apellido']
-        # tPersonal=request.form['tipoPersonal']
-        # sede=request.form['sede']
         Personal=request.form['tPersonal']
         tsede=request.form['tsede']
     try:
@@ -184,10 +167,6 @@ def preubasUpdate(id):
             f'{cdtls["user"]}/{cdtls["psswrd"]}@{cdtls["host"]}:{cdtls["port"]}/{cdtls["db"]}'
         )
         cur = connection.cursor()
-        # cur.execute(f"""SELECT IDSEDE FROM SEDE WHERE NOMBRESEDE='{sede}'""")
-        # __sede=cur.fetchall()[0][0]
-        # cur.execute(f"""SELECT IDROL FROM ROL WHERE DESCROL='{tPersonal}'""")
-        # __tPersonal=cur.fetchall()[0][0]
         query=f"""UPDATE PERSONAL SET IDROLFK='{Personal}', IDSEDEFK='{tsede}', NOMBREDOCENTE='{nombre}', APELLIDODOCENTE='{apellido}'
                         WHERE  IDPERSONAL='{id}'"""
         cur.execute(query)
